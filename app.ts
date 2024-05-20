98e7c26586aa37b498f05b960ef2eeb39493f41b
João Pedro <joaopedromouramendes1@gmail.com>
2024-05-20 12:08:27 -0300

    create: user model

--- app.ts
+++ app.ts
@@ -4,8 +4,8 @@ const app = express()
 
 const PORT = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+app.post('/', async (req: Request, res: Response) => {
+    res.send("Hello World")
 })
 
 app.listen(PORT, () => console.log(`Server iniciado em: http://localhost:${PORT}/`))
