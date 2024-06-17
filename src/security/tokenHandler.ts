2d1d030f66c8226691d600a3376291f59de3e37d
João Pedro <joaopedromouramendes1@gmail.com>
2024-06-17 15:05:55 -0300

    add: verifyToken method in TokenHadler

--- src/security/tokenHandler.ts
+++ src/security/tokenHandler.ts
@@ -4,11 +4,17 @@ import dotenv from "dotenv"
 dotenv.config()
 
 class TokenHandler {
+    SECRET = process.env.SECRET as string
+
     // Gera um token
     generateToken(data: object, options?: jwt.SignOptions) {
-        const SECRET = process.env.SECRET as string
-        const token = jwt.sign(data, SECRET, options)
+        const token = jwt.sign(data, this.SECRET, options)
         return token
+    }
+
+    // Verifica se o token é valido
+    verifyToken(token: string, callback?: jwt.VerifyCallback<string | jwt.JwtPayload>) {
+        jwt.verify(token, this.SECRET, callback)
     }
 }
 
